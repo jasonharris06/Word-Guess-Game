259cedef96a000fa10f9f9797a6658d23ccdfb8c
JazHar <jasonharris06@gmail.com>
2018-11-07 17:41:35 -0700

    working dashes

--- Word-Guess-Game/assets/javascript/game.js
+++ Word-Guess-Game/assets/javascript/game.js
@@ -1,16 +1,21 @@
 var userInput = "";
 var wins = 0;
 var loses = 0;
+var gamefinish = true;
 var artist={
         artist: ["areial","jason","greg"],
 
 }
 // Set up game
 
+$("#word").remove();
+var artistLetters = "";
+//set up dashes
 var randArtist = artist.artist[Math.floor(Math.random()*artist.artist.length)];
-for(var i = 1; i < randArtist.length;i++ ){
-         
-    console.log(randArtist.charAt(i))
-}
+for(var i = 0; i < randArtist.length; i++ ){
+    var artistLetters = ("_ " + artistLetters); 
     
+}
+
+console.log(artistLetters)
 
